8e59cb9b112401bd5f2580ea7306622488501f0c
Samir Katyar <samirkatiyar78685@gmail.com>
2022-07-09 10:03:38 +0530

    [master] * add cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 import { Logger as Pino } from 'nestjs-pino';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      methods: ['GET', 'POST', 'PUT'],
+      allowedHeaders: ['X-Access-Token', 'Content-Type'],
+      origin: true,
+    },
+  });
   app.useLogger(app.get(Pino));
   await app.listen(3000);
 }
